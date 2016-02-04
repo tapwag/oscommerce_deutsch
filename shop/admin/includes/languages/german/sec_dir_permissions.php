00c477d5d45b68b4988d7ed03c1043f566c244f7
Maik Wagner <maiktapwagner@aol.com>
2016-02-04 16:45:37 +0100

    Box in the main menu had two entries starting with Sicherheit. Changed the name of one item.

--- shop/admin/includes/languages/german/sec_dir_permissions.php
+++ shop/admin/includes/languages/german/sec_dir_permissions.php
@@ -10,9 +10,9 @@
   Released under the GNU General Public License
 */
 
-define('HEADING_TITLE', 'Verzeichnis Sicherheit/Lesezugriffsrecht');
+define('HEADING_TITLE', 'Zugriffsrechte bei Verzeichnissen');
 
-define('TABLE_HEADING_DIRECTORIES', 'Directories/Verzeichnisse');
+define('TABLE_HEADING_DIRECTORIES', 'Verzeichnisse');
 define('TABLE_HEADING_WRITABLE', 'Beschreibbar');
 define('TABLE_HEADING_RECOMMENDED', ' Vorgeschlagen');
 
