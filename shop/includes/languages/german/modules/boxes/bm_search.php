03953450e1076b943c10a03d16963ad22b43296e
Maik Wagner <maiktapwagner@aol.com>
2016-02-04 19:32:38 +0100

    Capitalization.

--- shop/includes/languages/german/modules/boxes/bm_search.php
+++ shop/includes/languages/german/modules/boxes/bm_search.php
@@ -14,5 +14,5 @@
   define('MODULE_BOXES_SEARCH_DESCRIPTION', 'Schnellsuche anzeigen');
   define('MODULE_BOXES_SEARCH_BOX_TITLE', 'Schnellsuche');
   define('MODULE_BOXES_SEARCH_BOX_TEXT', 'Verwenden Sie Stichworte, um ein Produkt zu finden.');
-  define('MODULE_BOXES_SEARCH_BOX_ADVANCED_SEARCH', 'erweiterte Suche');
+  define('MODULE_BOXES_SEARCH_BOX_ADVANCED_SEARCH', 'Erweiterte Suche');
 ?>
