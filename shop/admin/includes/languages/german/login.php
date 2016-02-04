dd869bdd0ee5aab996136fc2718caed829c2d8f9
Maik Wagner <maiktapwagner@aol.com>
2016-02-04 13:29:19 +0100

    Updated German l10n for administrator login.

--- shop/admin/includes/languages/german/login.php
+++ shop/admin/includes/languages/german/login.php
@@ -17,10 +17,9 @@ define('TEXT_PASSWORD', 'Password:');
 
 define('TEXT_CREATE_FIRST_ADMINISTRATOR', 'Es existieren keine Administratoren in der Datenbanktabelle. F&uuml;llen Sie bitte die folgende Informationen aus, um den ersten Verwalter herzustellen. (Ein manuelles Login wird noch nach diesem Schritt angefordert)');
 
-define('ERROR_INVALID_ADMINISTRATOR', 'Error: Unzul&auml;ssiger Administrator-Login-Versuch.');
+define('ERROR_INVALID_ADMINISTRATOR', 'Error: Unzul&auml;ssiger Administrator-Loginversuch.');
 
 define('BUTTON_LOGIN', 'Login');
 define('BUTTON_CREATE_ADMINISTRATOR', 'Administrator Erstellen');
-
-define('ERROR_ACTION_RECORDER', 'Fehler: Die maximale Anzahl von Login-Versuchen gemacht worden. Versuchen Sie es in %s Minuten.');
+define('ERROR_ACTION_RECORDER', 'Fehler: Die maximale Anzahl von Loginversuchen ist erreicht. Versuchen Sie es in %s Minuten noch einmal.');
 ?>
