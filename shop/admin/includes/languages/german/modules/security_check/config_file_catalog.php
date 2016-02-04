71e77c77efa064b1183afb7e1ade413fe68e0ceb
Maik Wagner <maiktapwagner@aol.com>
2016-02-04 16:18:45 +0100

    Altered security notice to indicate UNIX permissions of read-only

--- shop/admin/includes/languages/german/modules/security_check/config_file_catalog.php
+++ shop/admin/includes/languages/german/modules/security_check/config_file_catalog.php
@@ -12,5 +12,6 @@
 
 
 define('MODULE_SECURITY_CHECK_CONFIG_FILE_CATALOG_TITLE', 'Konfigurationsdatei');
-define('WARNING_CONFIG_FILE_WRITEABLE', 'Es ist m&ouml;glich in die Konfigurationsdatei zu schreiben: ' . DIR_FS_CATALOG . 'includes/configure.php. Dieses ist ein potentielles Sicherheitsrisiko - stellen Sie bitte die Benutzerrechte f&uuml;r diese Datei richtig ein.');
+define('WARNING_CONFIG_FILE_WRITEABLE', 'Es ist m&ouml;glich in die Konfigurationsdatei zu schreiben: ' . DIR_FS_CATALOG . 'includes/configure.php. Dieses ist ein potentielles Sicherheitsrisiko! Bitte stellen Sie die 
+Benutzerrechte f&uuml;r diese Datei richtig ein. (chmod 444)');
 ?>
